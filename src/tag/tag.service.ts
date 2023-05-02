64962bbfe4faafe70934fc1568e5b0f04c397758
abidkhan03 <abidkhan42935@gmail.com>
2023-05-03 02:22:04 +0500

    Create Nodemon File

--- src/tag/tag.service.ts
+++ src/tag/tag.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class TagService {
     findAll(): string[] {
-        return ['mongodb', 'mysql', 'mariadb', 'postgresql', 'sqlserver','sqlite'];
+        return ['mongodb', 'mysql', 'mariadb', 'postgresql', 'sqlserver'];
     }
 }
